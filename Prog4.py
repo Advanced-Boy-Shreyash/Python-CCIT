b12c990549ea03bd4128aebd9ef569a6938d047f
Shreyash <shreyashnandurkar2@gmail.com>
2022-11-06 16:04:59 +0530

    Added Prog5.py

--- Prog4.py
+++ Prog4.py
@@ -3,4 +3,4 @@
 a=int(input("Enter a no : "))
 b=a%100
 c=b//10
-print("Second Last Digt is ", c)
+print("Second Last Digit is ", c)
---
+++ Prog5.py
@@ -0,0 +1,8 @@
+# Mean of 3 Integer
+
+a = int(input("Enter a : "))
+b = int(input("Enter b : "))
+c = int(input("Enter c : "))
+
+mean=(a+b+c)/3
+print(f"Mean is {mean}")
