2088629044ec815fa6afcd370a33696d9bc4588e
Shreyash <shreyashnandurkar2@gmail.com>
2022-11-08 01:04:18 +0530

    Added Prog14.py

---
+++ Prog14.py
@@ -0,0 +1,10 @@
+# Gender is Male or Female
+
+g = input("Enter Your Gender Code : ")
+
+if g == "M" or g == "m":
+    print("Gender is Male")
+elif g == "F" or g == "f":
+    print("Gender is Female")
+else:
+    print("Enter Valid Gender")
