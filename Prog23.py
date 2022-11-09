dc59ed98394ab31609f16dc25fb952ece13048e0
Shreyash <shreyashnandurkar2@gmail.com>
2022-11-09 17:24:40 +0530

    Added Prog23.py

---
+++ Prog23.py
@@ -0,0 +1,20 @@
+# All no From 1 to 100 using While Loop
+
+a = 1
+while a <= 100:
+    print(a)
+    a += 1
+
+# All no From 1 to 100 in a Single Line using While Loop
+
+a = 1
+while a <= 100:
+    print(a, end=" ")
+    a += 1
+
+# All no From 1 to 100 by using Separator Comma (,) using While Loop
+
+a = 1
+while a <= 100:
+    print(a, end=",")
+    a += 1
