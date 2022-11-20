ad0d83afd8deea41ea61d9d0a821b4db858d9b08
Shreyash <shreyashnandurkar2@gmail.com>
2022-11-20 13:38:24 +0530

    Added Prog37.py

---
+++ Prog37.py
@@ -0,0 +1,52 @@
+# Function to Print 1 to 100 nos
+
+def num():
+    for i in range(1, 101):
+        print(i)
+
+
+num()
+
+# Function to Print 1 to 100 Even nos
+
+
+def num():
+    for i in range(1, 101):
+        if i % 2 == 0:
+            print(i)
+
+
+num()
+
+# Function to Print 1 to 100 Even nos
+
+
+def num():
+    for i in range(1, 101):
+        if i % 2 != 0:
+            print(i)
+
+
+num()
+
+# Function to Print no from 1 to Specified no
+
+
+def num(a):
+    for i in range(1, a+1):
+        print(i)
+
+
+num(50)
+
+
+# Function to Print Even no from 1 to Specified no
+
+
+def num(a):
+    for i in range(1, a+1):
+        if i % 2 == 0:
+            print(i)
+
+
+num(50)
