12f8f04dd66bb805eba143fb9a70f96bfd860993
Shreyash <shreyashnandurkar2@gmail.com>
2022-11-21 19:05:41 +0530

    Added Prog39.py

---
+++ Prog39.py
@@ -0,0 +1,41 @@
+# Functions to Print Area & Circumference of a Circle
+
+def area(r):
+    a = 3.14*r*r
+    print("Area is", a)
+
+
+def circumference(r):
+    c = 2*3.14*r
+    print("Circumference is", c)
+
+
+r = int(input("Enter Radius : "))
+area(r)
+circumference(r)
+
+# Function to Print Sum of no. upto Specified no
+
+
+def sum(a):
+    s = 0
+    for i in range(1, a+1):
+        s = s+i
+    print("Sum is", s)
+
+
+a = int(input("Enter No. : "))
+sum(a)
+
+# Function to Print a Factorial
+
+
+def fact(a):
+    f = 1
+    for i in range(1, a+1):
+        f = f*i
+    print("Factorial is : ", f)
+
+
+a = int(input("Enter No. : "))
+fact(a)
