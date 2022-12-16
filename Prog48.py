5866c0aad48c176c50bf5ee8fc552e2e49d6c242
Shreyash <shreyashnandurkar2@gmail.com>
2022-12-17 00:47:05 +0530

    Updated Prog48.py

--- Prog48.py
+++ Prog48.py
@@ -1,5 +1,19 @@
 # Function Decorator
 
+# def smart(func):
+    # def advfn(a,b):
+        # if a==0 or b==0:
+            # return "Cannot Divide by Zero"
+        # else:
+            # return "func"
+        # return advfn
+# 
+# @smart
+# def div(a,b):
+    # return a/b
+# 
+# print(div(10,3))
+
 def decorator(fn):
     def advfn():
         print("--------------------")
