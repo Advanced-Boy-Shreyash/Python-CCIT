2d5faab7826fc25ed2f9a796b5cfaa5fba21d103
Shreyash <shreyashnandurkar2@gmail.com>
2022-12-21 14:49:37 +0530

    Added Prog57.py

---
+++ Prog57.py
@@ -0,0 +1,13 @@
+# Lambda Function
+
+# Lambda Function to Accept 2 nos as argument and Return Greatest of them
+
+
+# def x(a, b): return a if a > b else b
+
+
+# print("Greatest no. is", x(5, 7))
+
+x=lambda a,b: a if a>b else b
+
+print("Greatest no. is",x(5,7))
